6538e608323fb182a4acde39ca424106d183f006
mdrobin73 <sojol73@gmail.com>
2024-07-26 12:10:26 +0600

    initialized project

---
+++ Note.js
@@ -0,0 +1,18 @@
+/*
+
+*** HTML Tags That You Must Know ***
+
+    01. paragraph => (p)
+    02. anchor => (a)
+    03. image => (img)
+    04. headings => (h1, h2, h3, h4, h5, h6)
+    05. inline => (span, b, s, u, i, label)
+    06. table => (table, tr, td)
+    07. div => (div + semantics)
+    08. lists => (li, ol, ul)
+    09. form => (form, input, select, radio)
+    10. button => (button)
+    11. media => (audio, video, iframe)
+    12. extra => (meta, link, title, script)
+
+*/
